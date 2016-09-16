aae672e6f20ab86ba1c7211a19a7f7540a601dda
Jakub <mikrut.jakub@gmail.com>
2016-09-16 10:27:13 +0200

    add empty? method to PresenceValidator

--- validations/presence_validator.rb
+++ validations/presence_validator.rb
@@ -2,11 +2,15 @@ module Validations
   class PresenceValidator < Validator
 
     def valid?(value)
-      !value.nil?
+      !empty?(value)
     end
 
     def errors_message
       "#{@attribute.to_s} can't be blank"
+    end
+
+    def empty?(value)
+      value.nil? || value.length == 0
     end
   end
 end
